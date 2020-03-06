55ea3dccae9506ca20d920b66dfba756961f427e
Josh Archer <jarcher@greenriver.edu>
2020-03-06 12:34:32 -0800

    Finished our implementation of prim's algorithm.

--- graphs/src/test/TestUndirectedGraphs.java
+++ graphs/src/test/TestUndirectedGraphs.java
@@ -11,14 +11,18 @@ public class TestUndirectedGraphs
     {
         UndirectedALGraph<Integer> numGraph = new UndirectedALGraph<>();
 
-        numGraph.addVertex(1, 2,3, 4, 5);
+        numGraph.addVertex(1, 2,3, 4, 5, 6, 7);
         numGraph.addEdges(
             new DirectedALGraph.Edge<>(1, 2, 1),
             new DirectedALGraph.Edge<>(2, 3, 3),
             new DirectedALGraph.Edge<>(2, 4, 2),
             new DirectedALGraph.Edge<>(1, 4, 1),
             new DirectedALGraph.Edge<>(2, 5, 2),
-            new DirectedALGraph.Edge<>(3, 4, 1)
+            new DirectedALGraph.Edge<>(3, 4, 1),
+            new DirectedALGraph.Edge<>(2, 6, 2),
+            new DirectedALGraph.Edge<>(3, 6, 2),
+            new DirectedALGraph.Edge<>(6, 7, 1),
+            new DirectedALGraph.Edge<>(1, 7, 2)
         );
 
         System.out.println("Vertex set size: " + numGraph.vertexSize());
